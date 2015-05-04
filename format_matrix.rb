b3d3e42911d77c540a182e187896d2f3f3ad8f88
Bosco So <boscoso@halcyon.local>
2015-05-04 13:57:56 -0400

    calculating CONSUMPTN and stripping HI & LO

--- format_matrix.rb
+++ format_matrix.rb
@@ -4,6 +4,8 @@ require_relative 'lib/matrix_formatter'
 
 converter = CsvConverter.new
 presult = converter.process(ARGF)
+consolidated = converter.consolidate_consumption(presult)
+stripped = converter.strip_channels(consolidated, "CONSUMPTN HI", "CONSUMPTN LO")
 
 formatter = MatrixFormatter.new
-formatter.output(STDOUT, presult)
+formatter.output(STDOUT, stripped)
